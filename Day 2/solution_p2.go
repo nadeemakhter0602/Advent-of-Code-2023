9cd4df268fde5c8aaa0c9a34a04c063b48fd6a45
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-12-03 10:20:15 +0530

    add day 2 part 2 solution

---
+++ Day 2/solution_p2.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func PanicErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getPower(line string) int {
+	game := strings.Split(line, " ")
+	colorMax := map[string]int{
+		"r": 0,
+		"g": 0,
+		"b": 0,
+	}
+	game = game[2:]
+	for idx, element := range game {
+		number, err := strconv.Atoi(element)
+		if err != nil {
+			continue
+		} else {
+			color := string(game[idx+1][0])
+			colorNumber, ok := colorMax[color]
+			if ok && number > colorNumber {
+				colorMax[color] = number
+			}
+		}
+	}
+	power := colorMax["r"] * colorMax["g"] * colorMax["b"]
+	return power
+}
+
+func getPowersSum(file *os.File) int {
+	sum := 0
+	line := ""
+	for true {
+		buffer := make([]byte, 1)
+		_, err := file.Read(buffer)
+		if err == io.EOF {
+			sum += getPower(line)
+			break
+		}
+		line += string(buffer[0])
+		PanicErr(err)
+		if buffer[0] == []byte{'\n'}[0] {
+			sum += getPower(line)
+			line = ""
+		}
+	}
+	return sum
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	PanicErr(err)
+	defer file.Close()
+	fmt.Println("PART 2:", getPowersSum(file))
+}
